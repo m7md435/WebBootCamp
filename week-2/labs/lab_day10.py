5651a42055eb0aadfed842234b98ff1e520de4d1
m7md435 <M7mdyf123@gmail.com>
2026-08-06 21:17:12 +0300

    m

---
+++ week-2/labs/lab_day10.py
@@ -0,0 +1,82 @@
+#lab 1
+
+for attempts in range(3):
+    print("Attempt", attempts + 1)
+
+#lab 2
+
+for num in range(2, 11, 2):
+    print(num)
+
+#lab 3
+
+for secondstolaunch in range(10, 0, -1):
+    print(f"T-:{secondstolaunch}")
+
+#lab 4
+
+course = "python"
+for letter in course:
+    print(letter)
+
+#lab 5 
+students = ["shahad", "khadija", "yamam"]
+for student in students:
+    print(f" Progressing student is: {student}") 
+
+#lab 6
+
+for number in range(1, 11):
+    if number % 2 == 0:
+        print(f"{number} is an even number")
+    else:
+        print(f"{number} is an odd number")
+    print("----------------------")
+
+
+#lab 7
+
+numbers = [1, 2, 3, 4, 5]
+even_counter = 0
+for number in numbers:
+    if number % 2 == 0:
+        even_counter += 1
+
+print(f"Total even numbers is: {even_counter}")
+
+
+#lab 8
+
+prices = [10, 20, 30, 40, 50]
+total = 0
+
+for price in prices:
+    total += price
+
+print(f"Total price is: {total} Vat: {total * 1.15:.2f}")
+
+
+#lab 9
+
+count = 1
+while count < 5:
+    count += 1
+    print(f"Count.... {count}")
+print("loop completed")
+
+
+#lab 10
+message = "Please enter your age: "
+age_text = input(message).strip()
+while not age_text.isdigit():
+    age_text = input(message).strip()
+age = int(age_text)
+print(f"Your age is: {age}")
+
+#lab 11
+password = "python123"
+print("Welcome to the system")
+
+while password == "":
+    password = input("Please enter the password: ")
+
