31f22cf2501f5600a460ff7b5803f4122e3ab843
m7md435 <M7mdyf123@gmail.com>
2026-08-10 20:08:16 +0300

    m

---
+++ week-3/labs/exercise_day12/main.py
@@ -0,0 +1,9 @@
+import grades
+
+def main():
+    print(grades.calculate_grade(95))
+    print(grades.calculate_grade(85))
+    print(grades.calculate_grade(75))
+
+if __name__ == "__main__":
+    main()
