3d2f12abc1a349b132284c28c0a96daa1aedfd5b
m7md435 <M7mdyf123@gmail.com>
2026-08-12 21:22:07 +0300

    m

--- week-3/labs/exercise_day14.py
+++ week-3/labs/exercise_day14.py
@@ -29,3 +29,49 @@ passing_scores = [
 ]
 
 print(passing_scores)  # [67, 91, 75]
+
+raw_names = [" sara ", " ", "omar", "  lina  "]
+cleaned_names = [
+    name.strip().title()  # expression
+    for name in raw_names
+    if name.strip() # clause
+]
+print(cleaned_names)  # ["sara", "omar", "lina"]
+
+for name in raw_names:
+    print(name.strip())
+    
+numbers = [1, 2]
+letters = ["a", "b"]
+combined = [
+    (number, letter)  # expression
+    for number in numbers
+    for letter in letters  # clause
+    ]
+print(combined)  # [(1, "a"), (1, "b"), (2, "a"), (2, "b")]
+
+scores = [42, 67, 91]
+labels = [
+    "pass" if score >= 60 else "retry"  # expression
+    for score in scores               # clause
+]
+print(labels)  # ["retry", "pass", "pass"]    
+
+email = [
+    "SARA@example.Com",
+    "omar@example.com",
+    "lina@school.sa"
+]
+domain = {
+    email.split("@")[1].lower() 
+    for email in email}
+print(domain)  # {"example.com"}
+
+numbers = range(1, 6)
+
+squares = {
+    number : number ** 2
+    for number in numbers
+}
+print(squares)  # {1: 1, 2: 4, 3: 9, 4: 16, 5: 25}
+
