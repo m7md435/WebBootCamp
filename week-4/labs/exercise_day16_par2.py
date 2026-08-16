6480f59f7f1e527fe0ff4ca468243c79a224be00
m7md435 <M7mdyf123@gmail.com>
2026-08-16 21:01:02 +0300

    m

--- week-4/labs/exercise_day16_par2.py
+++ week-4/labs/exercise_day16_par2.py
@@ -7,21 +7,31 @@ class Student:
              if score >= 0 and score <= 100:
                 self.score.append(score)
 
-        def __str__(self):
-            return f"Student Name: {self.name}, Score: {self.score} Average Score: {self.average_score():.2f}"
+class course:
+    def __init__(self, name):
+        self.name = name
+        self.students = []
 
-        def average_score(self):
-            if len(self.score) == 0:
-                return 0
-            return sum(self.score) / len(self.score)
+    def add_student(self, student):
+        if isinstance(student, Student):
+            self.students.append(student)
+        
+
+    def average_course_score(self):
+        if len(self.students) == 0:
+            return 0
+        return sum(student.score) / len(student.score)
+
+    def __str__(self):
+        return f"Course Name: {self.name}, Number of Students: {len(self.students)}, Average Course Score: {self.average_course_score():.2f}"
+
+course1 = course("Python")
+student1 = Student("sara", [80, 90])
+student2 = Student("omar", [70, 85])
+course1.add_student(student1)
+course1.add_student(student2)
+student1.add_score(100)
 
 
-course = [
-    Student("sara", [99, 65, 100]),
-    Student("omar", [88]),
-    Student("ali", [20,  80])
-
-]
-for student in course:
-    student.add_score(90)
-    print(student)
+for student in course1.students:
+    print(f"Student Name: {student.name}, Score: {student.score} , Average Score: {course1.average_course_score():.2f}")
